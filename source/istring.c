fcfd8b30e8f822f5e53c2aa149364767e7d7fe10
Paow <micaelloberg@gmail.com>
2014-10-06 16:18:48 +0200

    Added empty function bodys for all non-implemented functions

--- source/istring.c
+++ source/istring.c
@@ -109,3 +109,39 @@ size_t istrfixlen(char *istr) {
     istr[first_non_printable] = '\0';
     return first_non_printable-1;
 }
+
+
+/* 
+ * Sets the length of an istring and inserts a corresponding '\0'
+ * character. If the length argument is out of bounds for the string,
+ * a new istring should be returned with the contents of the original
+ * string. The last character of the original string will be repeated
+ * to fill the string to its given length.
+ */
+char* istrslen(char *istr, size_t length) { return NULL; }
+
+/*
+ * For definitions, see the manual page of respective function on the
+ * solaris system and the assignment text.
+ * 
+ * Think about whether having O(1) access to the length can be used to
+ * improve the implementations if the string.h equivalents and use
+ * that to guide your implementations!
+ */
+char *istrchr(const char *s, int c) { return NULL; }
+char *istrrchr(const char *s, int c) { return NULL; }
+int istrcmp(const char *s1, const char *s2) { return NULL; }
+int istrncmp(const char *s1, const char *s2, size_t n) { return NULL; }
+size_t istrlen(const char *s) { return NULL; }
+
+/*
+ * I nedanstående funktioner är dst en pekare till en vanlig
+ * sträng och inte en istring. Däremot skall minnesutrymmet
+ * "konverteras" till en istring av funktionerna, d.v.s. efter att
+ * t.ex. istrcpy anropats bör man vid anropsplatsen göra dst =
+ * STRING(dst) för att hoppa över längd-delen av strängen.
+*/
+char *istrcpy(char *dst, const char *src) { return NULL; }
+char *istrncpy(char *dst, const char *src, size_t n) { return NULL; }
+char *istrcat(char *dst, const char *src) { return NULL; }
+char *istrncat(char *dst, const char *src, size_t n) { return NULL; }
